6c68558c042761e4b69ce6a33cc81b4237657071
Josafat <josamtz@outlook.com>
2020-05-27 00:19:38 -0500

    🔥 MVP Architecture
    Create a simple MVP architecture with unit testing for iOS Application

---
+++ sampleMVParchitecture/Presenter/UserPresenter.swift
@@ -0,0 +1,11 @@
+//
+//  UserPresenter.swift
+//  sampleMVParchitecture
+//
+//  Created by Josafat Martínez  on 27/05/20.
+//  Copyright © 2020 Josafat Martínez . All rights reserved.
+//
+
+import Foundation
+class UserPresenter {
+}
