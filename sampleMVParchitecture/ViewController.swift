19c87828b90217cb4f5422c53ecbeedc05105267
Josafat <josamtz@outlook.com>
2020-05-27 00:05:52 -0500

    🚀 Project Structure

---
+++ sampleMVParchitecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sampleMVParchitecture
+//
+//  Created by Josafat Martínez  on 27/05/20.
+//  Copyright © 2020 Josafat Martínez . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
